753e71e0d8ae75c70606199eff3bc6c5f1f09393
Ginada <ginadam@gmail.com>
2015-09-29 16:59:01 +0200

    added a filter

---
+++ src/app.js
@@ -0,0 +1,18 @@
+'use strict';
+var app = angular.module('myFilters', [])
+	.filter('capitalize', function () {
+		return function (input) {
+			if (angular.isString(input)) {
+				//return 'hello';
+				return input.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+			} else {
+				return input;
+			}
+		};
+	})
+	.controller('myController', function($scope){
+		$scope.inputString = "beforE string"
+		$scope.validate = function(myString){
+			return myString;
+		};
+	});
